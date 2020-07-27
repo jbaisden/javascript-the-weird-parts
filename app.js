ac3b891cb0f94ad1e709f60d01b48948720bd330
Jason Baisden <jasonbaisden@gmail.com>
2020-07-26 21:14:15 -0500

    more notes.

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
-var person = {
-    firstname: 'default',
-    lastname: 'default',
-    getfullName: function() {
-        return this.firstname + ' ' + this.lastname;
-    }
+function Person() {
+    this.firstname = "Jane";
+    this.lastname = "Doe";
 }
 
-var john = {
-    firstname: 'John',
-    lastname: 'Doe'
-}
+var john = new Person();
 
-// DON'T DO THIS EVER - FOR DEMO PURPOSES ONLY!
-john.__proto__ = person;
-
-for(var prop in john) {
-    if(john.hasOwnProperty(prop)) {
-        console.log(prop + ': ' + john[prop]);
-    }
-}
-
-
-// console.log(john.getfullName());
-// console.log(john.firstname);
-
-// var jane = {
-//     firstname: 'Jane'
-// }
-// jane.__proto__ = person;
-// console.log(jane.getfullName());
+console.log(john);
+console.log(Person);
