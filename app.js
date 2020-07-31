1788f3360d63aefcb680106bc248829355553aa2
Jason Baisden <jasonbaisden@gmail.com>
2020-07-31 17:19:27 -0500

    updated course project

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 
-var q = $("ul.people li");
-console.log(q);
+// var g = G$('John', 'Doe');
+// console.log(g);
+// console.log(g.greet().setLanguage('es').greet(true));
+// console.log($);
+
+
+$(window).ready(function() {
+    $("#formalSelection").on('change',  function() {
+        console.log("changing greeting. Formal?: " + $('#formalSelection option:selected').val());
+        console.log( $('#formalSelection option:selected').val() );
+        var g = G$( $('#firstname').val(), $('#lastname').val(), $('#lang').val(), $('#formalSelection option:selected').val() );
+        g.HTMLGreeting('#greeting');
+        console.log(g);
+    });
+
+    $("#lang").on('change',  function() {
+        console.log("changing language. Lang: " + $('#lang').val() );
+        console.log( $('#lang').val() );
+        var g = G$( $('#firstname').val(), $('#lastname').val() , $('#lang').val(), $('#formalSelection  option:selected').val() );
+        g.HTMLGreeting('#greeting');
+    });    
+});
+
---
+++ project_library/Greetr.js
@@ -0,0 +1,125 @@
+;(function(global, $) {
+    var Greetr = function(firstname,lastname, language, formal) {
+        //if formal is string, cast to boolean
+        return new Greetr.init(firstname, lastname, language, formal);
+    }
+
+    var supportedLangs = ['en','es'];
+    greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    var logMessages = {
+        en: 'Logged in',
+        es: 'Inicio Sesion'
+    }
+
+    Greetr.prototype = {
+        fullname: function() {
+            return this.firstname + ' ' + this.lastname;
+        },
+
+        validate: function() {
+            if(supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid Language";
+            }
+        },
+
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstname;
+        },
+
+        formalGreeting: function() {
+            return formalGreetings[this.language] + ', ' + this.fullname();
+        },
+
+        log: function() {
+
+            if(console) {
+                console.log(logMessages[this.language] + ': ' + this.fullname());
+            }
+
+            //makes method chainable
+            return this;
+        },
+
+        greet: function(formal) {
+            var msg;
+
+            formal = !!formal;
+            console.log(formal);
+
+            if(formal) {
+                msg = this.formalGreeting();
+            } else {
+                msg = this.greeting();
+            }
+
+            if(console) {
+                console.log(msg);
+            }
+
+            //makes method chainable
+            return this;
+        },
+
+        setLanguage: function(newLang) {
+            this.language = newLang;
+            this.validate();
+
+            //makes method chainable
+            return this;
+        },
+
+        setFormal: function(formal) {
+            console.log(this.formal);
+            this.formal = !!formal; //coerce value to boolean
+            return this;
+        },
+
+        HTMLGreeting: function(selector) {
+
+            if(!$) {
+                throw 'jquery not loaded';
+            }
+
+            if(!selector) {
+                throw 'Missing selector';
+            }
+
+            var msg ;
+            if(this.formal) {
+                msg = 'formal: ' + this.formalGreeting();
+            } else {
+                msg = 'informal: ' + this.greeting();
+            }
+
+            $(selector).html(msg);
+
+            return this;
+        }
+
+    };
+
+    Greetr.init = function(firstname, lastname, language, formal) {
+        var self = this;
+        self.firstname = firstname || 'John';
+        self.lastname = lastname || 'Doe';
+        self.language = language || 'en';
+        self.formal = formal ;
+        console.log(self.formal);
+
+        self.validate();
+    }
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    global.Greetr = global.G$ = Greetr;
+
+}(window, jQuery));
